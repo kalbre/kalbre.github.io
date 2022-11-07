a65b7c084a654d938de21e17ea749bdc6557bca3
Kristin <Kristin.albrecht@ndus.edu>
2022-11-07 16:13:21 -0600

    Add files via upload

---
+++ New folder/code.js
@@ -0,0 +1,12 @@
+2 // intger literal
+3.14 //
+"Hello" // Text (string) literal
+'Hello'
+2+1 // expression can return the single result
+2-(1+3+5)
+2*8
+var data = 2*(1*(3+5)); //variable
+console.log(data);
+data = 110;
+console.log(data);
+
