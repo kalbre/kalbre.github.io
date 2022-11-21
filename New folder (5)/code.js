16971a78fd6d5eee3d24a979b71d5dc432e34c4a
Kristin <Kristin.albrecht@ndus.edu>
2022-11-20 19:12:07 -0600

    Add files via upload

--- New folder (5)/code.js
+++ New folder (5)/code.js
@@ -1,34 +1,46 @@
 var painter1 = document.getElementById("c1").getContext("2d");
 
-function drawBackground (painter) {
-    painter.fillStye = "#000000";
-    painter.fillRect(0, 0, 400, 400);
+function drawBackground (painter1) {
+    painter1.fillStyle = "#000000";
+    painter1.fillRect(0, 0, 400, 400);
 }
 drawBackground(painter1);
 
 painter1.fillStyle = "#ff0000";
 for (var j = 0; j < 13; ++j){
-    for (var i = 0; i<13; ++i) {
-    painter1.fillRect(10 + i*30, 10 + j*30, 20, 20);
+    for (var i = 0; i < 13; ++i) {
+    painter1.fillRect(10 + (2*i-j)*30, 370 - j*30, 20, 20);
     }
+  
 }
 
 var painter2 = document.getElementById("c2").getContext("2d");
 
-function drawBackground(painter) {
-    painter.fillStyle = "#000000";
-    painter.fillRect(0, 0, 400, 400);
+function drawBackground(painter2) {
+    painter2.fillStyle = "#000000";
+    painter2.fillRect(0, 0, 400, 400);
 }
 
 drawBackground(painter2);
 
-var k = 0;
-var x = 0;
+painter2.fillStyle = "#ff0000";
+var i=0;
 
-while (x < 13) {
-  
-    painter2.fillStyle = "#ff0000";
-    painter2.fillRect(10 + x*30, 370 - x*30, 20, 20);
-    var x = x++;
-    var k = k++;
+while (i < 13) {
+    var j = 0;
+    while (j < 13){
+    painter2.fillRect(10 + (i)*30, 370 - (j-i)*30, 20, 20);
+    j++;
+    }
+    i++;
+}
+var i = 0;
+painter2.fillStyle = "#000000";
+while (i < 13) {
+    var j = 0;
+    while (j < 13){
+    painter2.fillRect(10 + (j-i)*30, 370 - (j+1)*30, 20, 20);
+    j++;
+    }
+    i++;
 }
